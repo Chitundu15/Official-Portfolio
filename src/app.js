98cc908b261c10b631d9ea9c49f149681863b03e
Chitundu15 <72439288+Chitundu15@users.noreply.github.com>
2024-01-13 13:24:35 +0200

    Added nav bar

--- src/app.js
+++ src/app.js
@@ -2,3 +2,13 @@ document.addEventListener('mousemove', (e) => {
     document.body.style.setProperty('--x', e.clientX + 'px');
     document.body.style.setProperty('--y', e.clientY + 'px');
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.nav-links');
+
+    hamburger.addEventListener('click', function () {
+      navLinks.classList.toggle('active');
+      hamburger.classList.toggle('is-active');
+    });
+  });
