912a5a62c9b4628f00d0ff9e7a32e70831131284
Chitundu15 <72439288+Chitundu15@users.noreply.github.com>
2024-02-06 12:15:16 +0200

    Updated page links

--- src/app.js
+++ src/app.js
@@ -12,3 +12,16 @@ document.addEventListener('DOMContentLoaded', function () {
       hamburger.classList.toggle('is-active');
     });
   });
+
+  //Scroll to section handler
+  function scrollToSection(sectionId) {
+    var section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' });
+
+    // Optional: Add a class to highlight the active link
+    var links = document.querySelectorAll('a');
+    links.forEach(function (link) {
+        link.classList.remove('active');
+    });
+    document.querySelector('a[href="#' + sectionId + '"]').classList.add('active');
+}
